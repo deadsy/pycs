c64f6fa08d96937b8aa4425fb2a5bd9e20d39bc8
Jason Harris <jasonh@mistsys.com>
2016-05-24 20:39:33 -0700

    Added NXP/Freescale SVDs.

--- mk/svd2py.mk
+++ mk/svd2py.mk
@@ -5,8 +5,9 @@ PATCH = patch
 PY_FILES = $(patsubst %, %.py, $(FILES)) 
 
 %.py: ./svd/%.svd.gz
-	$(SVD2PY) $< $@
-	if [ -e $*.patch ]; then $(PATCH) -i $*.patch -b --suffix .original; fi;
+	@echo "[svd2py]" $*
+	@$(SVD2PY) $< $@
+	@if [ -e $*.patch ]; then $(PATCH) -i $*.patch -b --suffix .original; fi;
 
 all: $(PY_FILES)
 
