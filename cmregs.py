b19e5c428fc784900b272e684a98e83e5cf8a2a3
Jason Harris <jasonh@mistsys.com>
2016-06-02 13:37:22 -0700

    More system peripherals.

--- cmregs.py
+++ cmregs.py
@@ -11,6 +11,17 @@ SVD file will need to be appended with the system peripherals.
 We define the system peripherals here and then selectively add them to
 the device structure as part of the SoC fixup process.
 
+Typically:
+cm0* is for cm0 and cm0plus
+cm3* is for cm3 and cm4
+
+There are some minor register differences, and I'm not being overly precise.
+We reference the registers by name, so it's more important to have the
+right names in the right place so we can use common code to do system oriented
+things.
+
+Register and peripheral names are taken from the ARM documentation.
+
 """
 # -----------------------------------------------------------------------------
 
@@ -48,15 +59,6 @@ def _build_registers(reg_info):
 #  Nested Vectored Interrupt Controller
 
 # base_name, offset, number, description
-_cm3_nvic_info = (
-  ('ISER', 0x000, 8, '(R/W) Interrupt Set Enable Register'),
-  ('ICER', 0x080, 8, '(R/W) Interrupt Clear Enable Register'),
-  ('ISPR', 0x100, 8, '(R/W) Interrupt Set Pending Register'),
-  ('ICPR', 0x180, 8, '(R/W) Interrupt Clear Pending Register'),
-  ('IABR', 0x200, 8, '(R/W) Interrupt Active Bit Register'),
-  ('IPR', 0x300, 60, '(R/W) Interrupt Priority Register'),
-)
-
 _cm0_nvic_info = (
   ('ISER', 0x000, 1, '(R/W) Interrupt Set Enable Register'),
   ('ICER', 0x080, 1, '(R/W) Interrupt Clear Enable Register'),
@@ -64,6 +66,15 @@ _cm0_nvic_info = (
   ('ICPR', 0x180, 1, '(R/W) Interrupt Clear Pending Register'),
   ('IABR', 0x200, 1, '(R/W) Interrupt Active Bit Register'),
   ('IPR', 0x300, 8, '(R/W) Interrupt Priority Register'),
+)
+
+_cm3_nvic_info = (
+  ('ISER', 0x000, 8, '(R/W) Interrupt Set Enable Register'),
+  ('ICER', 0x080, 8, '(R/W) Interrupt Clear Enable Register'),
+  ('ISPR', 0x100, 8, '(R/W) Interrupt Set Pending Register'),
+  ('ICPR', 0x180, 8, '(R/W) Interrupt Clear Pending Register'),
+  ('IABR', 0x200, 8, '(R/W) Interrupt Active Bit Register'),
+  ('IPR', 0x300, 60, '(R/W) Interrupt Priority Register'),
 )
 
 def _build_nvic_registers(nvic_info):
@@ -89,8 +100,8 @@ def _build_nvic_peripheral(nvic_info):
   p.registers = _build_nvic_registers(nvic_info)
   return p
 
-cm3_nvic = _build_nvic_peripheral(_cm3_nvic_info)
 cm0_nvic = _build_nvic_peripheral(_cm0_nvic_info)
+cm3_nvic = _build_nvic_peripheral(_cm3_nvic_info)
 
 # -----------------------------------------------------------------------------
 # SysTick
@@ -115,12 +126,77 @@ systick = _p
 # -----------------------------------------------------------------------------
 # System Control Block
 
+# name, offset, description
+_cm0_scb_info = (
+  ('CPUID', 0x000, '(R/ ) CPUID Base Register'),
+  ('ICSR', 0x004, '(R/W) Interrupt Control and State Register'),
+  ('VTOR', 0x008, '(R/W) Vector Table Offset Register'),
+  ('AIRCR', 0x00C, '(R/W) Application Interrupt and Reset Control Register'),
+  ('SCR', 0x010, '(R/W) System Control Register'),
+  ('CCR', 0x014, '(R/W) Configuration Control Register'),
+  ('SHPR2', 0x01c, '(R/W) System Handlers Priority Registers'),
+  ('SHPR3', 0x020, '(R/W) System Handlers Priority Registers'),
+  ('SHCSR', 0x024, '(R/W) System Handler Control and State Register'),
+)
+
+_cm3_scb_info = (
+  ('CPUID', 0x000, '(R/ ) CPUID Base Register'),
+  ('ICSR', 0x004, '(R/W) Interrupt Control and State Register'),
+  ('VTOR', 0x008, '(R/W) Vector Table Offset Register'),
+  ('AIRCR', 0x00C, '(R/W) Application Interrupt and Reset Control Register'),
+  ('SCR', 0x010, '(R/W) System Control Register'),
+  ('CCR', 0x014, '(R/W) Configuration Control Register'),
+  ('SHPR1', 0x018, '(R/W) System Handlers Priority Registers'),
+  ('SHPR2', 0x01c, '(R/W) System Handlers Priority Registers'),
+  ('SHPR3', 0x020, '(R/W) System Handlers Priority Registers'),
+  ('SHCSR', 0x024, '(R/W) System Handler Control and State Register'),
+  ('CFSR', 0x028, '(R/W) Configurable Fault Status Register'),
+  ('HFSR', 0x02C, '(R/W) HardFault Status Register'),
+  ('DFSR', 0x030, '(R/W) Debug Fault Status Register'),
+  ('MMFAR', 0x034, '(R/W) MemManage Fault Address Register'),
+  ('BFAR', 0x038, '(R/W) BusFault Address Register'),
+  ('AFSR', 0x03C, '(R/W) Auxiliary Fault Status Register'),
+  ('ID_PFR0', 0x040, '(R/ ) Processor Feature Register'),
+  ('ID_PFR1', 0x044, '(R/ ) Processor Feature Register'),
+  ('ID_DFR0', 0x048, '(R/ ) Debug Feature Register'),
+  ('ID_ADR0', 0x04C, '(R/ ) Auxiliary Feature Register'),
+  ('ID_MMFR0', 0x050, '(R/ ) Memory Model Feature Register'),
+  ('ID_MMFR1', 0x054, '(R/ ) Memory Model Feature Register'),
+  ('ID_MMFR2', 0x058, '(R/ ) Memory Model Feature Register'),
+  ('ID_MMFR3', 0x05c, '(R/ ) Memory Model Feature Register'),
+  ('ID_ISAR0', 0x060, '(R/ ) Instruction Set Attributes Register'),
+  ('ID_ISAR1', 0x064, '(R/ ) Instruction Set Attributes Register'),
+  ('ID_ISAR2', 0x068, '(R/ ) Instruction Set Attributes Register'),
+  ('ID_ISAR3', 0x06c, '(R/ ) Instruction Set Attributes Register'),
+  ('ID_ISAR4', 0x070, '(R/ ) Instruction Set Attributes Register'),
+  ('CPACR', 0x088, '(R/W) Coprocessor Access Control Register'),
+  ('STIR', 0x200, '( /W) Software Trigger Interrupt Register'),
+)
+
+def _build_scb_peripheral(info):
+  p = soc.peripheral()
+  p.name = 'SCB'
+  p.description = 'System Control Block'
+  p.address = SCB_BASE
+  p.size = None
+  p.default_register_size = 32
+  p.registers = _build_registers(info)
+  return p
+
+cm0_scb = _build_scb_peripheral(_cm0_scb_info)
+cm3_scb = _build_scb_peripheral(_cm3_scb_info)
 
 # -----------------------------------------------------------------------------
 # Memory Protection Unit
 
 
+
+
+
 # -----------------------------------------------------------------------------
 # Floating Point Unit
+
+
+
 
 # -----------------------------------------------------------------------------
