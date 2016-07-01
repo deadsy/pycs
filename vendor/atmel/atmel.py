7055dc8b6a50bee222447ba11248e20ac3571de7
Jason Harris <jasonh@mistsys.com>
2016-07-01 08:09:22 -0700

    Atmel: Working on flash driver.

--- vendor/atmel/atmel.py
+++ vendor/atmel/atmel.py
@@ -11,6 +11,7 @@ Run fixup functions to correct any SVD inadequecies.
 
 import soc
 import cmregs
+import util
 
 #-----------------------------------------------------------------------------
 # build a database of SoC devices
@@ -20,6 +21,43 @@ class soc_info(object):
     pass
 
 soc_db = {}
+
+#-----------------------------------------------------------------------------
+# NVM User Row Mapping: Not in the SVD :-(
+
+def _eeprom_fmt(x):
+  return '%s' % util.memsize((1 << (14-x),0)[x == 7])
+
+def _bootprot_fmt(x):
+  return '%s' % util.memsize((1 << (15-x),0)[x == 7])
+
+_nvmr0_fieldset = (
+  ('WDT_Period', 31, 28, None, 'WDT Period at power-on'),
+  ('WDT_Always', 27, 27, None, 'WDT Always-On at power-on'),
+  ('WDT_Enable', 26, 26, None, 'WDT Enable at power-on'),
+  ('BOD12_Action', 25, 24, None, 'BOD12 Action at power-on'),
+  ('BOD12_Disable', 23, 23, None, 'BOD12 Disable at power-on'),
+  ('BOD12_Level', 22, 17, None, 'BOD12 threshold level at power-on'),
+  ('BOD33_Action', 16, 15, None, 'BOD33 Action at power-on'),
+  ('BOD33_Disable', 14, 14, None, 'BOD33 Disable at power-on'),
+  ('BOD33_Level', 13, 8, None, 'BOD33 threshold level at power-on'),
+  ('EEPROM', 6, 4, _eeprom_fmt, 'Used to select one of eight different EEPROM sizes'),
+  ('BOOTPROT', 2, 0, _bootprot_fmt, 'Used to select one of eight different bootloader sizes'),
+)
+
+_nvmr1_fieldset = (
+  ('LOCK', 31, 16, None, 'NVM Region Lock Bits'),
+  ('BOD12_Hysteresis', 10, 10, None, 'BOD12 Hysteresis configuration Hysteresis at power-on'),
+  ('BOD33_Hysteresis', 9, 9, None, 'BOD33 Hysteresis configuration Hysteresis at power-on'),
+  ('WDT_WEN', 8, 8, None, 'WDT Timer Window Mode Enable at power-on'),
+  ('WDT_EWOFFSET', 7, 4, None, 'WDT Early Warning Interrupt Time Offset at power-on'),
+  ('WDT_Window', 3, 0, None, 'WDT Window mode time-out at power-on'),
+)
+
+_nvm_user_row_regset = (
+  ('NVMUR0', 32, 0x0, _nvmr0_fieldset, 'NVM User Row 0'),
+  ('NVMUR1', 32, 0x4, _nvmr1_fieldset, 'NVM User Row 1'),
+)
 
 #-----------------------------------------------------------------------------
 # ATSAML21J18B
@@ -32,6 +70,7 @@ def ATSAML21J18B_fixup(d):
   d.insert(soc.make_peripheral('rww',  0x00400000, 8 << 10, None, 'RWW Section'))
   d.insert(soc.make_peripheral('sram', 0x20000000, 32 << 10, None, 'SRAM'))
   d.insert(soc.make_peripheral('lp_sram', 0x30000000, 8 << 10, None, 'Low Power SRAM'))
+  d.insert(soc.make_peripheral('NVMUR', 0x00804000, 8, _nvm_user_row_regset, 'NVM User Row'))
 
 s = soc_info()
 s.name = 'ATSAML21J18B'
--- vendor/atmel/flash.py
+++ vendor/atmel/flash.py
@@ -7,6 +7,9 @@ Notes:
 1) Instantiating this driver does not touch the hardware. No reads or writes.
 We only access the hardware when the user wants to do something.
 
+2) The erase unit is a row. The write unit is a page. There are 4 pages per row.
+Each page is 64 bytes.
+
 """
 #-----------------------------------------------------------------------------
 
@@ -16,10 +19,10 @@ import util
 import mem
 
 #-----------------------------------------------------------------------------
-# Define the pages of flash memory for various devices
+# Define the rows of flash memory for various devices
 
 ATSAML21J18B_flash = (
-  ('flash', (64,) * 4096),
+  ('flash', (256,) * 1024),
 )
 
 # map device.soc_name to the flash map
@@ -34,11 +37,11 @@ class flash(object):
   def __init__(self, device):
     self.device = device
     self.hw = self.device.NVMCTRL
-    self.pages = mem.flash_regions(self.device, flash_map[self.device.soc_name])
+    self.rows = mem.flash_regions(self.device, flash_map[self.device.soc_name])
 
   def sector_list(self):
-    """return a list of flash pages"""
-    return self.pages
+    """return a list of flash rows"""
+    return self.rows
 
   def check_region(self, x):
     """return None if region x meets the flash write requirements"""
@@ -61,6 +64,6 @@ class flash(object):
     pass
 
   def __str__(self):
-    return util.display_cols([x.col_str() for x in self.pages])
+    return util.display_cols([x.col_str() for x in self.rows])
 
 #-----------------------------------------------------------------------------
