bd97d1925ac7c8a86298e66843a932da43609505
Jason Harris <jasonh@mistsys.com>
2016-07-07 16:12:35 -0700

    Most of the SVD files don't validate against the xsd for trivial reasons. The soc.py is generally tolerant, but it chokes on these files and I'm not inclined to fix them at this point.

--- mk/svd2py.mk
+++ mk/svd2py.mk
@@ -1,10 +1,13 @@
 
 SVD2PY = $(TOP)/svd2py
+XSD =  $(TOP)/vendor/arm/CMSIS-SVD_Schema_1_1_draft.xsd
 
 SVD_FILES = $(wildcard ./svd/*.svd.gz)
 PY_FILES = $(patsubst ./svd/%.svd.gz, %.py, $(SVD_FILES)) 
 
 %.py: ./svd/%.svd.gz
+	@#echo "[xmllint]" $*
+	@#xmllint --noout --schema $(XSD) $<
 	@echo "[svd2py]" $*
 	@$(SVD2PY) $< $@
 
