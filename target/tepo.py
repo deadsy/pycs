b4e3a741c6d865897d77e435e99fdb059b457815
Jason Harris <jasonh@mistsys.com>
2016-07-11 11:47:48 -0700

    tepo: presumptive pin assignments.

--- target/tepo.py
+++ target/tepo.py
@@ -27,27 +27,105 @@ soc_name = 'EFM32LG890F128'
 prompt = 'tepo'
 
 # -----------------------------------------------------------------------------
-# gpio configuration
+# gpio configuration: see -http://hackingthepo.weebly.com/
 
 gpio_cfg = (
-
-  # PA0
-  # PA1
-  # PA2
-  # PA3
-  # PA4
-  # PA5
-  # PA6
-  # PA7
-  # PA8
-  # PA9
-  # PA10
-  # PA11
+  # PA0 lcd seg 13
+  # PA1 led col 3
+  # PA2 lcd seg 15
+  # PA3 led col 2
+  # PA4 lcd seg 17
+  # PA5 led row 1
+  # PA6 led col 1
+  # PA7 lcd seg 35
+  # PA8 lcd seg 36
+  # PA9 lcd seg 37
+  # PA10 lcd seg 38
+  # PA11 lcd seg 39
   # PA12
   # PA13
   # PA14
-  # PA15
-
+  # PA15 lcd seg 12
+  # PB0 lcd seg 32
+  # PB1 led col 0
+  # PB2 lcd seg 34
+  # PB3 led row 2
+  # PB4 lcd seg 21
+  # PB5 lcd seg 22
+  # PB6 lcd seg 23
+  # PB7
+  # PB8
+  # PB9 key 15
+  # PB10 key 11
+  # PB11 key 6
+  # PB12 key 14
+  # PB13
+  # PB14
+  # PB15 led row 3
+  # PC0 led row 0
+  # PC1 key sound
+  # PC2 key 1
+  # PC3 key 5
+  # PC4 key 9
+  # PC5 key 13
+  # PC6 key 16
+  # PC7 key 4
+  # PC8 key 12
+  # PC9 key play
+  # PC10 key 8
+  # PC11 key style
+  # PC12 key oo
+  # PC13 codec reset
+  # PC14 input common
+  # PC15
+  # PD0 codec sdin (us1_tx_1)
+  # PD1 codec sdout (us1_rx_1)
+  # PD2 codec sclk (us1_clk_1)
+  # PD3 codec lrck (us1_cs_1)
+  # PD4 right pot (adc0_ch4)
+  # PD5 left pot (adc0_ch5)
+  # PD6 codec sda (i2c0_sda_1)
+  # PD7 codec scl (i2c0_scl_1)
+  # PD8 key 7
+  # PD9 lcd seg 28
+  # PD10 lcd seg 29
+  # PD11 lcd seg 30
+  # PD12 lcd seg 31
+  # PD13 key 10
+  # PD14 key pattern
+  # PD15 key 2
+  # PE0 key 3
+  # PE1 key bpm
+  # PE2 codec mclk (tim3_cc2_1)
+  # PE3 key write
+  # PE4 lcd com 0
+  # PE5 lcd com 1
+  # PE6 lcd com 2
+  # PE7 lcd com 3
+  # PE8 lcd seg 4
+  # PE9 lcd seg 5
+  # PE10 lcd seg 6
+  # PE11 lcd seg 7
+  # PE12 lcd seg 8
+  # PE13 lcd seg 9
+  # PE14 lcd seg 10
+  # PE15 lcd seg 11
+  # PF0
+  # PF1
+  # PF2 lcd seg 0
+  # PF3 lcd seg 1
+  # PF4 lcd seg 2
+  # PF5 lcd seg 3
+  # PF6 lcd seg 24
+  # PF7 lcd seg 25
+  # PF8 lcd seg 26
+  # PF9 lcd seg 27
+  # PF10
+  # PF11
+  # PF12
+  # PF13
+  # PF14
+  # PF15
 )
 
 # -----------------------------------------------------------------------------
