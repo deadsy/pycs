40df281058616e3032aa855b148d9645e5889b14
Jason Harris <jasonh@mistsys.com>
2016-07-27 10:21:21 -0700

    Working asm2py utility.

--- vendor/st/lib/build_lib.sh
+++ vendor/st/lib/build_lib.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 ASM2PY=../../../tools/asm2py
+LIB=../lib.py
 
-rm lib.py
+rm $LIB
 
-$ASM2PY stm32f4_flash.S >> lib.py
-
-rm *.o
-rm *.elf
-rm *.bin
-
+$ASM2PY stm32f4_flash.S >> $LIB
+$ASM2PY stm32f2x.S >> $LIB
